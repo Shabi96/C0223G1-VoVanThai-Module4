6e3944a9317606f590b40cafbeb649e014a66df0
Shabi96 <vuvivokiller@gmail.com>
2023-07-04 19:43:20 +0700

    em nộp bài tập ngày 4/7

---
+++ ss14_spring_security/security/spring-security-example/src/main/java/com/example/entity/AppUser.java
@@ -0,0 +1,62 @@
+package com.example.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "App_User", //
+        uniqueConstraints = { //
+                @UniqueConstraint(name = "APP_USER_UK", columnNames = "User_Name") })
+public class AppUser {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "User_Id", nullable = false)
+    private Long userId;
+
+    @Column(name = "User_Name", length = 36, nullable = false)
+    private String username;
+
+    @Column(name = "Encryted_Password", length = 128, nullable = false)
+    private String password;
+
+    @Column(name = "Enabled", length = 1, nullable = false)
+    private boolean enabled;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String encrytedPassword) {
+        this.password = encrytedPassword;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+}
